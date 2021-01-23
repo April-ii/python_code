4c3df51d267717adb4f21294c213c7c984b65e4e
April-ii <1659299826@qq.com>
2021-01-23 22:06:33 +0800

    py2

--- 二级/draft.py
+++ 二级/draft.py
@@ -1,12 +1,15 @@
 fi=open("卖火柴的小女孩.txt",'r')
+fo=open("PY.txt",'w')
 txt=fi.read()
 d={}
-exclude='，。！？、（）【】《》<>=：+-*—“”…'
 for word in txt:
-    if word in exclude: continue
-    else:
-        d[word]=d.get(word,0)+1
+    d[word]=d.get(word,0)+1
+del d["\n"]
+del d[" "]
 ls=list(d.items())
 ls.sort(key=lambda x:x[1],reverse=True)
-print("{}:{}".format(ls[0][0],ls[0][1]))
+for i in range(len(ls)):
+    ls[i]="{}:{}".format(ls[i][0],ls[i][1])
+fo.write(",".join(ls))
 fi.close()
+fo.close()
